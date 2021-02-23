3f8d261a3538430c1882565f99b153ef6916dbac
tujony <70181220+tujony@users.noreply.github.com>
2021-02-23 00:12:11 -0600

    Update CalculadoraCod.java

--- CalculadoraCod.java
+++ CalculadoraCod.java
@@ -1,159 +1,161 @@
 
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Iterator;
-import java.util.Stack;
-
 
 /**
-*
-* @author Juan Andres Galicia Reyes 20298 UVG 2021
-* @version 06/02/21
-*/
-public class CalculadoraCod implements calculadora{
-	private int resultado;
-	
-/**
- * @param x primer numero
- * @implSpec y segundo numero
- * @return la suma de ambos
+ *
+ * @author Jonathan Espinoza
+ * @version 4-2-2021
  */
-	public int suma(int x, int y) {
+public class ProtCalculadora implements calculadora {
+    int resultado;
+    /**
+     * 
+     * @param x
+     * @param y
+     * @return 
+     */
+    @Override
+    public int suma(int x, int y) {
+        //Suma 2 valores
         resultado = x + y;
         return resultado;
     }
 /**
  * 
- * @param x primer numero
- * @param y segundo numero
- * @return  la resta de ambos de forma y-x
+ * @param x
+ * @param y
+ * @return 
  */
     @Override
     public int resta(int x, int y) {
+        //Resta 2 valores
         resultado = y - x;
         return resultado;
     }
     /**
      * 
-     * @param x primer numero
-     * @param y segundo numero
-     * @return la multiplicacion de ambos
+     * @param x
+     * @param y
+     * @return 
      */
     @Override
     public int multiplicacion(int x, int y) {
+        //Multiplica 2 valores
         resultado = x * y;
         return resultado;
     }
     /**
      * 
-     * @param x primer numero
-     * @param y segundo numero
-     * @return  la division de ambos de forma y/x
+     * @param x
+     * @param y
+     * @return 
      */
     @Override
     public int division(int x, int y) {
+        //Divide 2 valores
         resultado = y/x;
         return resultado;
     }
-
-	@Override
-	public int operar(Stack x) {
-		Stack<Integer> pila2 = new Stack<>();
-		Iterator value = x.iterator(); 
-		while (value.hasNext()) {
-            if(x.peek().equals("+"))
-            {
-            	
-            	System.out.println(pila2);
-            	int y = (int) pila2.pop();
-            	int z = (int) pila2.pop();
-            	int resultado = suma(y,z);
-            	System.out.println(z+" + "+y+"="+resultado);
-            	pila2.push(resultado);
-            	x.pop();
+    /**
+     * 
+     * @param x
+     * @return 
+     */
+    @Override
+    public int operar(Stack x) {
+        //No implementado
+        return 0; 
+    }
+    /**
+     * 
+     * @param a
+     * @return 
+     */
+    @Override
+    public String decode(String a) {
+        //Crear pila y calculadora
+        calculadora cal;
+        MyStack<Integer> Stack;
+        
+        cal = new ProtCalculadora();
+        Stack = new MyStack<>();
+    
+        String res = null;
+        String pros = " ";
+        String datos;
+        
+            
+        datos = a;
+        //Verificar que no este vacio el archivo
+        while (datos!=null){
+               
+               String partes[] = datos.split(" ");
+               for (int i = 0; i < partes.length; i++) {
+                   System.out.println(partes[i]);
             }
-            else if(x.peek().equals("-"))
-            {
-            	System.out.println(pila2);
-            	
-            	int y = (int) pila2.pop();
-            	int z = (int) pila2.pop();
-            	int resultado = resta(y,z);
-            	System.out.println(z+" - "+y+"="+resultado);
-            	pila2.push(resultado);
-            	x.pop();
+               //Leer cada linea del archivo
+               for (int i=0; i < partes.length; i++){
+                   //Si operador = +
+                   if ((partes[i].equals("+"))){
+                       int resultado;
+                       int x = Stack.pop();
+                       int y = Stack.pop();
+                       resultado = cal.suma(x, y);
+                       Stack.push(resultado); 
+                       pros = pros + ("\n" + partes[i] +"\tSumar: pop, pop y push del resultado " + "\t" + resultado);
+                    }
+                   //Si operador = -
+                   else if((partes[i].equals("-"))){
+                       int resultado;
+                       int x = Stack.pop();
+                       int y = Stack.pop();
+                       resultado = cal.resta(x, y);
+                       Stack.push(resultado);  
+                       pros = pros + ("\n" + partes[i] + "\tResta: pop, pop y push del resultado " + "\t" + resultado);
+                   }
+                   //Si operador = *
+                   else if((partes[i].equals("*"))){
+                       int resultado;
+                       int x = Stack.pop();
+                       int y = Stack.pop();
+                       resultado = cal.multiplicacion(x, y);
+                       Stack.push(resultado); 
+                       //String resultante
+                       pros = pros + ( "\n" +  partes[i] + "\tMultiplicar: pop, pop y push del resultado "  + "\t" + resultado);
+                   }
+                   //Si operador = /
+                   else if((partes[i].equals("/"))){
+                       int resultado;
+                       int x = Stack.pop();
+                       int y = Stack.pop();
+                       resultado = cal.division(x, y);
+                       Stack.push(resultado); 
+                       pros = pros + ("\n" + partes[i] + "\tDividir: pop, pop y push del resultado " + "\t" + resultado);
+                   }
+                  
+                   else{
+                       //En caso que la entrada sea un numero
+                       if(Stack.size() < 1){
+                        pros = pros + ("\n" + partes[i] + "\tPush operando" + "\t" + partes[i] );
+                       }
+                       else if(Stack.size() >= 1){
+                        pros = pros + ("\n" + partes[i] + "\tPush operando" + "\t" + Stack.peek() + ", " + partes[i] );
+                       }
+                       
+                       Stack.push(Integer.parseInt(partes[i]));
+                       
+                       
+                  }
+               }
+               //String resultante completo
+               res = ("RESULTADO: " + Stack.peek() + "\n" + "ENTRADA" + "\t OPERACION" + "\t\t PILA");
+               res = res + "\n "+ pros;
+               datos = null;
+               
             }
-            else if(x.peek().equals("*"))
-            {
-            	System.out.println(pila2);
-            	
-            	int y = (int) pila2.pop();
-            	int z = (int) pila2.pop();
-            	int resultado = multiplicacion(y,z);
-            	System.out.println(z+" * "+y+"="+resultado);
-            	pila2.push(resultado);
-            	x.pop();
-            }
-            else if(x.peek().equals("/"))
-            {
-            	System.out.println(pila2);
-            	
-            	int y = (int) pila2.pop();
-            	int z = (int) pila2.pop();
-            	int resultado = division(y,z);
-            	System.out.println(z+" / "+y+"="+resultado);
-            	pila2.push(resultado);
-            	x.pop();
-            }
-            else
-            {
-            	int y = Integer.parseInt((String) x.pop());
-        		pila2.push(y);
-        		System.out.println(pila2.peek());
-            }
-        }
-        System.out.println(pila2);
-		return pila2.pop();
-	}
-
-	@Override
-	/**
-	 * metodo que forma la primera pila leyendo el archivo de texto
-	 * @param cadena de texto del archivo
-	 * @return cadena de texto con el resultado
-	 */
-	public String decode(String a) {
-		Stack<String> aux = new Stack<>();
-		Stack<String> pila = new Stack<>();
-		int total=0;
-		String res = "";
-		try
-		{
-			FileReader read = new FileReader(a);
-            BufferedReader read1 = new BufferedReader(read);
-            String data = read1.readLine();
-            while(data != null)
-            {
-            	String sep[] = data.split(" ");
-                
-                for (int i=0; i < sep.length; i++){
-                	aux.push(sep[i]);
-                }
-                
-                data = read1.readLine();
-                Iterator<String> it = aux.iterator();
-                while (it.hasNext()) {
-                    pila.push(aux.pop());
-                }
-                System.out.println(pila);
-                total = operar(pila);
-            }
-		}
-		catch(ArithmeticException | IOException | NumberFormatException e){
-            return e.toString();
-		}
-		return Integer.toString(total);
-	}
+        return res;
+    }
+    
 }
